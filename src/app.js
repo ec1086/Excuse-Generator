34fd0aa48c6433be98cb45be7fa641cf065aad64
ec1086 <ec1086@gmail.com>
2022-06-02 00:57:03 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,34 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = myExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let myExcuse = () => {
+  let who = ["The cat", "My son", "His donkey", "His dinosaur"];
+  let action = ["burned", "took", "stole", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the storm.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was flying."
+  ];
+  /*Random*/
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  /*innerHtml*/
+  let excuse = document.getElementById("excuse");
+  /*Function new excuse*/
+
+  return (
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + " "
+  );
 };
